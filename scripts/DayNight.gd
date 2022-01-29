ae8f1a3b67b876c6d495d220942c14b378e13179
troido <troido@protonmail.com>
2022-01-29 13:48:01 +0100

    play music at night

--- scripts/DayNight.gd
+++ scripts/DayNight.gd
@@ -24,16 +24,32 @@ func is_night():
 	return not is_day()
 
 
-
-func _on_DayEnd_timeout():
+func turn_night():
+	if not day:
+		return
 	day = false
-	$NightEnd.start()
 	get_tree().call_group("dual", "nightstart")
 	get_tree().call_group("dual", "daychange", true)
+	$NightMusic.play(0.0)
 
+func _input(event):
+	if Input.is_action_just_pressed("skip"):
+		if day:
+			turn_night()
+		else:
+			turn_day()
 
-func _on_NightEnd_timeout():
+func turn_day():
+	if day:
+		return
 	day = true
 	$DayEnd.start()
 	get_tree().call_group("dual", "daystart")
 	get_tree().call_group("dual", "daychange", false)
+	$NightMusic.stop()
+
+func _on_DayEnd_timeout():
+	turn_night()
+
+func _on_NightMusic_finished():
+	turn_day()
