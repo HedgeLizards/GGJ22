647d4ce562ffe5299cac507c5075e68fe3ccc724
Julian Vos <julian.vos@ixly.nl>
2022-01-29 15:49:36 +0100

    zoom in to fill the window if needed

--- scripts/Camer.gd
+++ scripts/Camer.gd
@@ -10,7 +10,14 @@ extends Camera2D
 func _ready():
 	limit_right = get_node("/root/Main/RightEdge").position.x
 	limit_bottom = get_node("/root/Main/BottomEdge").position.y
+	
+	get_viewport().connect('size_changed', self, 'change_zoom')
+	change_zoom()
 
+func change_zoom():
+	var ratio = Vector2(2049, 1686) / OS.window_size
+	
+	zoom = min(min(ratio.x, ratio.y), 1.5) * Vector2.ONE
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
