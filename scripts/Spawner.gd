458af6aa4883923e3d4537ddde7d58d4b7de8260
troido <troido@protonmail.com>
2022-01-29 11:19:08 +0100

    fix bunny spawning

--- scripts/Spawner.gd
+++ scripts/Spawner.gd
@@ -12,12 +12,12 @@ func _on_Spawner_timeout():
 	if get_tree().get_nodes_in_group("bunnies").size() < max_rabbits:
 		
 		var bunny = Rabbit.instance()
+		get_node("/root").add_child(bunny)
 		
-		if randf() < 0.5 or true:
+		if randf() < 0.5:
 			bunny.global_position.x = get_node("/root/Main/LeftEdge").position.x #- outside_spawn_distance
 		else:
 			bunny.global_position.x = get_node("/root/Main/RightEdge").position.x + outside_spawn_distance
 		var top_y = get_node("/root/Main/TopEdge").position.y + 5
 		var bottom_y = get_node("/root/Main/BottomEdge").position.y
 		bunny.global_position.y = top_y + randf() * (bottom_y - top_y)
-		get_node("/root").add_child(bunny)
