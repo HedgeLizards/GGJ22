3c97175cf5d756647f16e9158b59df5ba4f5e82d
Julian Vos <julian.vos@ixly.nl>
2022-01-30 16:22:57 +0100

    skip the Cutscene on web

--- scripts/Start.gd
+++ scripts/Start.gd
@@ -2,7 +2,10 @@ extends VBoxContainer
 
 func _input(event):
 	if event is InputEventMouseButton && event.button_index == BUTTON_LEFT && !event.pressed:
-		get_tree().change_scene_to(preload('res://scenes/Cutscene.tscn'))
+		if OS.has_feature('web'):
+			get_tree().change_scene_to(preload('res://scenes/Main.tscn'))
+		else:
+			get_tree().change_scene_to(preload('res://scenes/Cutscene.tscn'))
 
 func _on_Play_mouse_entered():
 	$Play.modulate = Color(111, 208, 35, 255) / 255.0
