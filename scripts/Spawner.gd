af666d60607f87159f9cc7407953f2fa2ec7ae97
troido <troido@protonmail.com>
2022-01-30 11:12:12 +0100

    less flying bunnies

--- scripts/Spawner.gd
+++ scripts/Spawner.gd
@@ -20,7 +20,7 @@ func _on_Spawner_timeout():
 			bunny.global_position.x = get_node("/root/Main/LeftEdge").position.x #- outside_spawn_distance
 		else:
 			bunny.global_position.x = get_node("/root/Main/RightEdge").position.x + outside_spawn_distance
-		var top_y = get_node("/root/Main/TopEdge").position.y + 5
+		var top_y = get_node("/root/Main/TopEdge").position.y + 30
 		var bottom_y = get_node("/root/Main/BottomEdge").position.y
 		bunny.global_position.y = top_y + randf() * (bottom_y - top_y)
 		start(max(1, delay - ndays))
